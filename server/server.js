144b82b89681031a825db0fb7ccfea9288fb3cb4
Datorman <mikeshutov@gmail.com>
2019-01-19 06:38:35 -0500

    Basic templateing engine

--- server/server.js
+++ server/server.js
@@ -23,7 +23,7 @@ io.on('connection', (socket) => {
     });
 
     socket.on('createLocationMessage', (coords) => {
-        io.emit('newLocationMessage', generateLocationMessage('admin', coords.latitude, coords.longitude))
+        io.emit('newLocationMessage', generateLocationMessage('Admin', coords.latitude, coords.longitude))
     });
     socket.on('disconnect', () => {
         console.log('User disconnected');
