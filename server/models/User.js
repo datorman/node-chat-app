aac9f5b941eddf6e7820155ad986e5de5c67b353
Mike Shutov <mikeshutov@gmail.com>
2019-03-27 11:03:15 -0400

    Salting passwords and setting up tokens for stateless auth

--- server/models/User.js
+++ server/models/User.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const {Schema} = mongoose;
+const bcrypt = require('bcryptjs');
 
 const UserSchema = new Schema({
     name:{
@@ -11,9 +12,33 @@ const UserSchema = new Schema({
     email:{
         type: String,
         unique: true
-    }
+    },
+    tokens:[{
+        access: {
+            type: String,
+            required : true
+        },
+        token: {
+            type: String,
+            required : true
+        }
+    }]
 });
 
+UserSchema.pre('save', function(next){
+    var user = this;
+    if(user.isModified('password')){
+        bcrypt.genSalt(10, (err, salt) => {
+            bcrypt.hash(user.password, salt , (err,hash) => {
+                user.password =  hash;
+                next();
+            });
+        }); 
+    } else {
+        next();
+    };
+
+});
 var User = mongoose.model('User',UserSchema);
 module.exports = {
     User
