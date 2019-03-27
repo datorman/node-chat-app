f591e56b2bb576b8fbe732f4a5a6b28fb896834d
Mike Shutov <mikeshutov@gmail.com>
2019-03-27 11:22:19 -0400

    Adding auth token generation on create f user

--- server/config/dev.js
+++ server/config/dev.js
@@ -1,3 +1,4 @@
 module.exports = {
     mongoURI: 'mongodb://127.0.0.1:27017/chat-app',
+    JWT_SECRET:'asbd1k2jn31j23njk12l3n21l3'
 };
--- server/routes/UserRoutes.js
+++ server/routes/UserRoutes.js
@@ -3,15 +3,16 @@ const {User} = require('./../models/User');
 module.exports = (app) => {
     app.post('/api/users/create', (req,res) => {
         User.findOne({'email':req.body.params.email}).then((user) => {
-            console.log(req.body.params.email);
             if(!user){
                 var _user = new User({
                     name: req.body.params.name,
                     email: req.body.params.email,
                     password: req.body.params.password
                 });
-                _user.save().then((newUser) => {
-                    res.send(newUser);
+                _user.save().then(() => {
+                    return _user.generateAuthToken();
+                }).then((token) => {
+                    res.header('x-auth',token).send({_user});
                 });
             } else {
                 res.status(400).send({'error':'user already exists'})
